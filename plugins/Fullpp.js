f91157a346e9d2f8ea914c44b0b1ce43e4e51938
Rafimayo <mrafimayo6@gmail.com>
2025-07-13 14:51:51 +0500

    Update Fullpp.js

--- plugins/Fullpp.js
+++ plugins/Fullpp.js
@@ -15,7 +15,7 @@ cmd({
     if (!isQuotedImage) { 
       return m.reply('⚠️ *please reply to an img or mention any photo..*'); 
     } 
-    m.reply('⏳ *𝐘𝐎𝐔𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 𝐔𝐏𝐃𝐀𝐓𝐈𝐍𝐆 𝐁𝐘 𝐉𝐄𝐑𝐑𝐘-𝐍𝐃, please wait...*'); 
+    m.reply('⏳ *𝐘𝐎𝐔𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 𝐔𝐏𝐃𝐀𝐓𝐈𝐍𝐆 𝐁𝐘 𝐉𝐄𝐑𝐑𝐘-𝐌𝐃, please wait...*'); 
     const imageBuffer = await m.quoted.download(); 
     const image = await Jimp.read(imageBuffer); 
     const buffer = await image.getBufferAsync(Jimp.MIME_JPEG); 
