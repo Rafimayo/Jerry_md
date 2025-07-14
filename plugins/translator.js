5253d51541549c84978e844f1f14290e7ad76776
Rafimayo <mrafimayo6@gmail.com>
2025-07-14 07:40:51 +0500

    Update translator.js

--- plugins/translator.js
+++ plugins/translator.js
@@ -23,7 +23,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const translation = response.data.responseData.translatedText;
 
         const translationMessage = `
-🌍 *SILENT-SOBX-MD TRANSLATION* 🌍
+🌍 *JERRY-MD TRANSLATION* 🌍
 
 🔤 *Original*: ${textToTranslate}
 
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
 🌐 *Language*: ${targetLang.toUpperCase()}
 
-*SILENT-SOBX-MD CREATION*`;
+*JERRY-MD CREATION*`;
 
         return reply(translationMessage);
     } catch (e) {
